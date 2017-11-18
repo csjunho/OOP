f465a90ac8c4bf2fd1ac26da3e8aaac489007b4e
csjunho <32322428+csjunho@users.noreply.github.com>
2017-11-18 22:43:53 +0900

    Add files via upload
    학생 정식을 Jungsik 테이블에 크롤링&인서트하는 소스입니다

---
+++ JungSik.py
@@ -0,0 +1,94 @@
+
+# -*- coding: utf-8 -*-
+
+
+from bs4 import BeautifulSoup
+
+from urllib.request import urlopen
+
+import pymysql
+import datetime
+
+html = urlopen('http://fd.postech.ac.kr/bbs/board_menu.php?bo_table=weekly')
+
+source = html.read()
+
+html.close()
+
+# MySQL Connection 연결
+conn = pymysql.connect(host='13.124.175.104', user='root', password='daisy7962', db='cafeteria', charset='utf8')
+
+curs = conn.cursor()
+
+sql = "select * from Menu"
+curs.execute(sql)
+
+rows = curs.fetchall()
+print(rows) 
+
+conn.close()
+
+
+soup = BeautifulSoup(source, "lxml")
+
+temp3=soup.get_text()
+
+start='(월)\n\n'
+
+end = 'Kcal'
+
+mon = (temp3.split(start))[1].split(end)[0]
+
+
+start='(화)\n\n'
+
+end = 'Kcal'
+
+tue = (temp3.split(start))[1].split(end)[0]
+
+
+start='(수)\n\n'
+
+end = 'Kcal'
+
+wed = (temp3.split(start))[1].split(end)[0]
+
+start='(목)\n\n'
+
+end = 'Kcal'
+
+thu = (temp3.split(start))[1].split(end)[0]
+
+start='(금)\n\n'
+
+end = 'Kcal'
+
+fri = (temp3.split(start))[1].split(end)[0]
+
+start='(토)\n\n'
+
+end = 'Kcal'
+
+sat = (temp3.split(start))[1].split(end)[0]
+
+start='(일)\n\n'
+
+end = 'Kcal'
+
+sun = (temp3.split(start))[1].split(end)[0]
+
+temp4=(temp3.split(start))[1].split(end)[0]
+
+def insert_jungsik(Mon,Tue,Wed,Thu,Fri,Sat,Sun,Week):
+    query = "INSERT INTO Jungsik (Mon,Tue,Wed,Thu,Fri,Sat,Sun,Week) " \
+            "VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+    args = (Mon,Tue,Wed,Thu,Fri,Sat,Sun,Week)
+    conn2 = pymysql.connect(host='13.124.175.104', user='root', password='daisy7962', db='cafeteria', charset='utf8')
+    cursor = conn2.cursor()
+    cursor.execute(query,args)
+    conn2.commit()
+    cursor.close()
+    conn.close()
+
+insert_jungsik(mon,tue,wed,thu,fri,sat,sun,datetime.datetime.today())
+    
